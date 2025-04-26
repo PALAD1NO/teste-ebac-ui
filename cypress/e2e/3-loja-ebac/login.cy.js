65b388e1e0646796c5714b5a880be86bf0f4ecde
Felipe Tarcilio <felipe.tarcilio@gmail.com>
2025-04-26 21:34:28 +0100

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login',()=>{
+
+it('Deve fazer login com sucesso',()=>{
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username').type('teste.fyl@teste.com.br')
+    cy.get('#password').type('teste@1234')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, teste.fyl (não é teste.fyl? Sair)')
+
+})
+
+})
