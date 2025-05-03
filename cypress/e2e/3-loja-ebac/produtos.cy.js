8e1d2b03293d22a51f602e5a9db6acdf1ba1497a
Felipe Tarcilio <felipe.tarcilio@gmail.com>
2025-05-03 01:02:28 +0100

    produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')        
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.eq(0)
+            .contains('Abominable Hoodie')
+            .click()
+        cy.get('#tab-title-description > a').should('exist')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
